961b6f9efc1cff54a64394f56782a5c9cb5fbf74
JD Cumpson <jdc@pagerduty.com>
2016-07-15 14:16:49 -0400

    Adding integration model and support in service model.

---
+++ pypd/models/integration.py
@@ -0,0 +1,80 @@
+# Copyright (c) PagerDuty.
+# See LICENSE for details.
+from .entity import Entity
+from ..errors import (InvalidEndpointOperation, InvalidArguments,
+                      InvalidEndpoint)
+
+
+class Integration(Entity):
+    """PagerDuty note entity."""
+
+    ALLOWED_INTEGRATION_TYPES = [
+        # 'aws_cloudwatch_inbound_integration',
+        'aws_cloudwatch_inbound_integration_reference',
+        # 'cloudkick_inbound_integration',
+        'cloudkick_inbound_integration_reference',
+        # 'event_transformer_api_inbound_integration',
+        'event_transformer_api_inbound_integration_reference',
+        # 'generic_email_inbound_integration',
+        'generic_email_inbound_integration_reference',
+        # 'generic_events_api_inbound_integration',
+        'generic_events_api_inbound_integration_reference',
+        # 'keynote_inbound_integration',
+        'keynote_inbound_integration_reference',
+        # 'nagios_inbound_integration',
+        'nagios_inbound_integration_reference',
+        # 'pingdom_inbound_integration',
+        'pingdom_inbound_integration_reference',
+        # 'sql_monitor_inbound_integration',
+        'sql_monitor_inbound_integration_reference',
+    ]
+
+    @classmethod
+    def validate(cls, integration_info):
+        """Validate `integration_info` to be valid integration data."""
+        assert isinstance(integration_info, dict)
+        assert (integration_info['type'] in cls.ALLOWED_INTEGRATION_TYPES)
+
+    @classmethod
+    def fetch(cls, id, service=None, endpoint=None, *args, **kwargs):
+        """Customize fetch because it lives on a special endpoint."""
+        if service is None and endpoint is None:
+            raise InvalidArguments(service, endpoint)
+
+        if endpoint is None:
+            sid = service['id'] if isinstance(service, Entity) else service
+            endpoint = '/incidents/{0}/integrations/{1}'.format(sid, id)
+
+        getattr(Entity, 'fetch').im_func(cls, endpoint=endpoint, *args,
+                                         **kwargs)
+
+    @classmethod
+    def delete(*args, **kwargs):
+        """Disable this endpoint, not valid v2."""
+        raise InvalidEndpoint('Not a valid location on this endpoint')
+
+    def remove(self, *args, **kwargs):
+        """Disable this operation, not valid on this endpoint."""
+        raise InvalidEndpointOperation(
+            'Not a valid operation on this endpoint.'
+        )
+
+    @classmethod
+    def create(cls, service=None, endpoint=None, data=None, *args, **kwargs):
+        """
+        Create an integration within the scope of an service.
+
+        Make sure that they should reasonably be able to query with an
+        service or endpoint that knows about an service.
+        """
+        cls._validate(data)
+
+        if service is None and endpoint is None:
+            raise InvalidArguments(service, endpoint)
+        if endpoint is None:
+            sid = service['id'] if isinstance(service, Entity) else service
+            endpoint = '/services/{0}/integrations'.format(sid)
+
+        # otherwise endpoint should contain the service path too
+        getattr(Entity, 'create').im_func(cls, endpoint=endpoint, data=data,
+                                          *args, **kwargs)
--- pypd/models/service.py
+++ pypd/models/service.py
@@ -1,11 +1,64 @@
 # Copyright (c) PagerDuty.
 # See LICENSE for details.
-from pypd.models.entity import Entity
+from .entity import Entity
+from .integration import Integration
+from .vendor import Vendor
 
 
 class Service(Entity):
     """PagerDuty service entity."""
-    STR_OUTPUT_FIELDS = ('id', 'name',)
 
-    def add_integration():
-        pass
+    STR_OUTPUT_FIELDS = ('id', 'name',)
+    integrationFactory = Integration
+    vendorFactory = Vendor
+
+    ALLOWED_SERVICE_TYPES = [
+        # 'service',
+        'service_reference',
+    ]
+
+    @classmethod
+    def validate(cls, service_info):
+        """Validate `service_info` to be acceptable service data."""
+        assert isinstance(service_info, dict)
+        assert (service_info['type'] in cls.ALLOWED_SERVICE_TYPES)
+
+    def create_integration(self, integration_info):
+        """
+        Create an integration for this incident.
+
+        See: https://v2.developer.pagerduty.com/v2/page/api-reference#!/
+              Services/post_services_id_integrations
+        """
+        self.integrationFactory.validate(integration_info)
+        service_info = integration_info.get('service')
+        vendor_info = integration_info.get('vendor')
+
+        if service_info is not None:
+            self.__class__.validate(service_info)
+
+        if vendor_info is not None:
+            self.vendorFactory.validate(vendor_info)
+
+        endpoint = '/'.join((self.endpoint, self.id, 'integrations'))
+        return self.integrationFactory.create(
+            endpoint=endpoint,
+            api_key=self.api_key,
+            data={'integration': integration_info, },
+        )
+
+    def integrations(self):
+        """Retrieve all this services integrations."""
+        ids = [ref['id'] for ref in self['integrations']]
+        return map(Integration.fetch, ids)
+
+    def get_integration(self, id):
+        """Retrieve a single integration by id."""
+        return Integration.fetch(id)
+
+    def update_integration(self, *args, **kwargs):
+        """Update this integration on this service."""
+        raise NotImplemented
+
+    # sugar-pills
+    view_integration = get_integration
