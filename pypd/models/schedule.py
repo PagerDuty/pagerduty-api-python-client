39d90c5a07a8d0b161cce64940458e2a86beaf58
Jeremy Logan <jeremy.logan@crowdstrike.com>
2017-10-18 14:03:33 -0500

    adding ability to query a Schedule's oncall users

--- pypd/models/schedule.py
+++ pypd/models/schedule.py
@@ -5,3 +5,10 @@ from .entity import Entity
 
 class Schedule(Entity):
     """PagerDuty schedule entity."""
+
+    def get_oncall(self, **kwargs):
+        """Retrieve this schedule's "on call" users."""
+
+        endpoint = '/'.join((self.endpoint, self.id, 'users'))
+
+        return self.request('GET', endpoint=endpoint, query_params=kwargs)
